c658b057ea221436ae539ffe7efa05d86b827250
Alair Tavares <alair.tavares@avalara.com>
2018-01-24 16:12:55 -0200

    first commit

---
+++ ConsoleApp1/Fundamentacao.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class ValorDets
+    {
+        public string ItemDet { get; set; }
+        public string ValorDet { get; set; }
+        public string CodigoCest { get; set; }
+        public string DescricaoDet { get; set; }
+        public string Categoria { get; set; }
+        public string TipoIva { get; set; }
+        public string ObedeceDestino { get; set; }
+        public string Embalagem { get; set; }
+        public string UnidadeMedida { get; set; }
+        public string QuantidadeDe { get; set; }
+        public string QuantidadeAte { get; set; }
+        public string VigenciaInicial { get; set; }
+        public string VigenciaFinal { get; set; }
+        public string Fabricante { get; set; }
+        public string Cnpj { get; set; }
+        public string Classe { get; set; }
+        public string Observacao { get; set; }
+        public List<string> Ncms { get; set; }
+        public string AtivadadeDet { get; set; }
+        public string DestinacaoDet { get; set; }
+        public string FinalidadeDet { get; set; }
+        public string TratativaDet { get; set; }
+    }
+    class Detalhe
+    {
+        public List<ValorDets> ValorDets;
+    }
+    class DetalheFundamentacao
+    {
+        public List<Detalhe> Detalhe { get; set; }
+    }
+
+    class Fundamentacao
+    {
+        public string Localizador { get; set; }
+        public string Descricao { get; set; }
+        public string Ato { get; set; }
+        public string NumeroAto { get; set; }
+        public string Ano { get; set; }
+        public string Artigo { get; set; }
+        public string Alinea { get; set; }
+        public string Item { get; set; }
+        public string Inciso { get; set; }
+        public string Letra { get; set; }
+        public string Paragrafo { get; set; }
+        public string ItemParagrafo { get; set; }
+        public string Anexo { get; set; }
+        public string Observacao { get; set; }
+        public string VigenciaInicio { get; set; }
+        public string VigenciaFim { get; set; }
+        public List<string> UFSOrigem { get; set; }
+        public List<string> UFSDestino { get; set; }
+        public string Esfera { get; set; }
+        public string Tributo { get; set; }
+        public string Especie { get; set; }
+        public List<string> Aliquotas { get; set; }
+        public string AtividadeFund { get; set; }
+        public string DestinacaoFund { get; set; }
+        public string FinalidadeFund { get; set; }
+        public string TratativaFund { get; set; }
+        public DetalheFundamentacao DetalheFundamentacao { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialDateTime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.fff");
+
+            //string filename = "./TAXADVISER_Oficial_MG_16 01 2018V3.xml";
+            string filename = "./TAXADVISER_TESTECARGA_2.xml";
+            //https://www.demacmedia.com/xml-parsing-using-linq/
+            var obj = ParseXml(filename);
+
+            obj.ForEach(item => {
+                Console.WriteLine(item.Localizador);
+                Console.WriteLine("----");
+            });
+
+            string finalDateTime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.fff");
+
+            Console.WriteLine($"Início: {initialDateTime}");
+            Console.WriteLine($"Fim: {finalDateTime}");
+
+            Console.ReadKey();
+        }
+
+        static void parse(XmlReader reader)
+        {
+            
+            List<string> nodes = new List<string>();
+            while (reader.Read())
+            {
+                if (reader.NodeType != XmlNodeType.Element || reader.IsEmptyElement)
+                {
+                    continue;
+                }
+
+                //Console.WriteLine(reader.Name);
+
+                switch (reader.Name)
+                {
+                    case "FUNDAMENTACAO":
+                        Console.WriteLine(reader.Name);
+                        nodes.Add(reader.Name);
+                        break;
+                    case "UFORIGEM":
+                        Console.WriteLine(reader.ReadString());
+                        break;
+                }
+                
+
+                //Console.WriteLine(reader.ReadString());
+                //Console.WriteLine(reader.Name);
+                //nodes.Add(reader.Name);
+                //reader.MoveToContent();
+                //parse(reader);
+            }
+
+            Console.WriteLine(nodes.Count);
+        }
+
+        static public List<Fundamentacao> ParseXml(string inputFilePath)
+        {
+            var fundamentacao = new List<Fundamentacao>();
+            using (var xmlReader = new StreamReader(inputFilePath))
+            {
+                var doc = XDocument.Load(xmlReader);
+
+                fundamentacao = doc.Descendants("FUNDAMENTACAO")
+                    .Select(item => new Fundamentacao {
+                        Localizador = item.Element("LOCALIZADOR").Value,
+                        Descricao = item.Element("DESCRICAO").Value,
+                        Ato = item.Element("ATO").Value,
+                        NumeroAto = item.Element("NUMEROATO").Value,
+                        Ano = item.Element("ANO").Value,
+                        Artigo = item.Element("ARTIGO").Value,
+                        Alinea = item.Element("ALINEA").Value,
+                        Item = item.Element("ITEM").Value,
+                        Inciso = item.Element("INCISO").Value,
+                        Letra = item.Element("LETRA").Value,
+                        Paragrafo = item.Element("PARAGRAFO").Value,
+                        ItemParagrafo = item.Element("ITEMPARAGRAFO").Value,
+                        Anexo = item.Element("ANEXO").Value,
+                        Observacao = item.Element("OBSERVACAO").Value,
+                        VigenciaInicio = item.Element("VIGENCIAINICIO").Value,
+                        VigenciaFim = item.Element("VIGENCIAFIM").Value,
+                        UFSOrigem = item.Descendants("UFSORIGEM")
+                            .Where(uf => uf != null && uf.Element("UFORIGEM") != null)
+                            .Select(uf => uf.Element("UFORIGEM").Value)
+                            .ToList(),
+                        UFSDestino =  item.Descendants("UFSDESTINO")
+                            .Where(uf => uf != null && uf.Element("UFDESTINO") != null)
+                            .Select(uf => uf.Element("UFDESTINO").Value)
+                            .ToList(),
+                        Esfera = item.Element("ESFERA").Value,
+                        Tributo = item.Element("TRIBUTO").Value,
+                        Especie = item.Element("ESPECIE").Value,
+                        Aliquotas = item.Descendants("ALIQUOTAS")
+                            .Where(aliquota => aliquota != null && aliquota.Element("VALORALIQUOTA") != null)
+                            .Select(aliquota => aliquota.Element("VALORALIQUOTA").Value)
+                            .ToList(),
+                        //AtividadeFund = item.Element("ATIVIDADEFUND").Value,
+                        //DestinacaoFund = item.Element("DESTINACAOFUND").Value,
+                        //FinalidadeFund = item.Element("FINALIDADEFUND").Value,
+                        //TratativaFund = item.Element("TRATATIVAFUND").Value,
+                        //DetalheFundamentacao
+                    })
+                    .ToList();
+            }
+            return fundamentacao;
+        }
+    }
+}
